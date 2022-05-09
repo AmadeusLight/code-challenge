210249427bbc66de62d4385b7e88c7a94870dc4f
AmadeusLight <70249580+AmadeusLight@users.noreply.github.com>
2022-05-09 16:35:28 -0700

    posible, si

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,40 @@
+const ExplorerService = require("../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/Reader");
+const explorers = Reader.readJsonFile("test/data/visualpartners_test.json");
+
+describe("Unit Tests for ExplorerService Class", () => {
+    test('1) Filter Explorers By Mission', () => {
+
+      const explorers = [{mission:"node"}]
+
+      const explorersInNode = ExplorerService.filterByMision();
+
+      expect(explorersInNode.length).toBe(1)
+
+
+    });
+  
+    test("2) Test applyValidationNumber", () => {
+        const trick1 = FizzbuzzService.applyValidationNumber(1);
+        const trick2 = FizzbuzzService.applyValidationNumber(3);
+        const trick3 = FizzbuzzService.applyValidationNumber(5);
+        const trick4 = FizzbuzzService.applyValidationNumber(15);
+
+        expect(trick1).toBe(1);
+        expect(trick2).toBe("fizz");
+        expect(trick3).toBe("buzz");
+        expect(trick4).toBe("fizzbuzz");
+    });
+});
+
+test("1) Test apply Validation Number", () => {
+    const trick1 = FizzbuzzService.applyValidationInNumber(1);
+    const trick2 = FizzbuzzService.applyValidationInNumber(3);
+    const trick3 = FizzbuzzService.applyValidationInNumber(5);
+    const trick4 = FizzbuzzService.applyValidationInNumber(15);
+
+    expect(trick1).toBe(1);
+    expect(trick2).toBe("FIZZ");
+    expect(trick3).toBe("BUZZ");
+    expect(trick4).toBe("FIZZBUZZ");
+});
